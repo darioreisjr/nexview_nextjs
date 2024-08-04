fec7725a02c978459072263aa503e946edc30436
Dario Reis <guttyerresjunior1995@gmail.com>
2024-08-04 12:00:00 -0300

    feat: install shadcn UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-2xl">Projeto Dashboard</h1>
+      <Button>Cadastrar novo produto</Button>
     </main>
   );
 }
